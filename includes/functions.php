70f51283a0016b6b456d79a1cc8c4217cd721cce
Edi Amin <to.ediamin@gmail.com>
2018-07-25 21:18:19 +0600

    Add two helper function to format SQL queries

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,34 @@
 <?php
+
+/**
+ * Log last wpdb query
+ *
+ * @since 1.0.0
+ *
+ * @param bool $echo
+ *
+ * @return void
+ */
+function dokan_dev_last_query( $echo = false ) {
+    global $wpdb;
+
+    dokan_dev_format_query( $wpdb->last_query, $echo );
+}
+
+/**
+ * Format the whitespace in a SQL string to make it easier to read
+ *
+ * @since 1.0.0
+ *
+ * @param string $query
+ * @param bool   $echo
+ *
+ * @return void
+ */
+function dokan_dev_format_query( $query, $echo = false ) {
+    if ( $echo ) {
+        echo SqlFormatter::format( $query );
+    } else {
+        error_log( print_r( "\n" . SqlFormatter::format( $query, false ), true ) );
+    }
+}
